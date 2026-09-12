1dc96300b124267324d05c02af5f788643ed4d30
Luke Manyamazi <lukemanyamazi1@gmail.com>
2026-09-12 19:05:50 +0200

    security: remove Firebase credential logging from server startup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
-
 import dotenv from "dotenv";
 dotenv.config();
 
-import { firebaseConfig ,auth} from "./services/firebaseConfig.js";
+import "./services/firebaseConfig.js";
 
-console.log(process.env.FIREBASE_API_KEY);
-console.log(firebaseConfig);
-console.log("API Key:", process.env.FIREBASE_API_KEY); 
+console.log("Prepaid Water eTopUp server starting...");
